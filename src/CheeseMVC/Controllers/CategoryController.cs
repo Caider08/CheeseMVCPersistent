ba2a0c3a63296336be4b07699eed15ebdb6e08eb
Caider08 <caidbuch@ksu.edu>
2017-04-09 21:28:11 -0500

    finished step 1

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CheeseMVC.Models;
+using CheeseMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.ViewModels;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<CheeseCategory> categories = context.Categories.ToList();
+            return View(categories);
+
+        }
+
+        public IActionResult Add()
+        {
+            AddCategoryViewModel adds = new AddCategoryViewModel();
+
+            return View(adds);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addcategory)
+        {
+            if (ModelState.IsValid)
+            {
+                CheeseCategory newCategory = new CheeseCategory
+                {
+                    Name = addcategory.Name,
+       
+                };
+
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+                return Redirect("/");
+
+            }
+            return View(addcategory);
+        }
+
+
+
+    }
+}
---
+++ src/CheeseMVC/Models/CheeseCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CheeseMVC.Models
+{
+    public class CheeseCategory
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+
+    }
+}
